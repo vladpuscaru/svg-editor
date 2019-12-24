fca3c14b70f6f8ff3a147e454e702fe49bba4c2a
Vlad-Cristian Puscaru <vlad.cpuscaru@gmail.com>
2019-12-24 17:33:35 +0200

    Desktop UI finished

--- 6_1082_PUSCARU_Vlad.js
+++ 6_1082_PUSCARU_Vlad.js
@@ -1,14 +1,37 @@
 $(document).ready(function () {
 
+  let activeItem = $(".item.active");
+
   // UI events
   $("#btn").click(function () {
+    if ($(this).hasClass('rotated')) {
+      setTimeout(function () {
+        $("#info").removeClass('animated--fast fadeOut');
+        $("#copy").removeClass('animated--fast fadeOut');
+        $("#toolbar").removeClass('animated--fast fadeOut');
+        $("#info").addClass('animated fadeIn active');
+        $("#copy").addClass('animated fadeIn active');
+        $("#toolbar").addClass('animated fadeIn active');
+      }, 500);
+    } else {
+      setTimeout(function () {
+        $("#info").removeClass('active');
+        $("#copy").removeClass('active');
+        $("#toolbar").removeClass('active');
+      }, 500);
+      $("#info").removeClass('animated fadeIn');
+      $("#copy").removeClass('animated fadeIn');
+      $("#toolbar").removeClass('animated fadeIn');
+      $("#info").addClass('animated--fast fadeOut');
+      $("#copy").addClass('animated--fast fadeOut');
+      $("#toolbar").addClass('animated--fast fadeOut');
+    }
+
     $(this).toggleClass('rotated');
     $("#sidebar").toggleClass('swipeRight');
     $("#sidebar").toggleClass('swipeLeft');
-    $("#info").toggleClass('fadeIn');
-    $("#info").toggleClass('fadeOut');
-    $("#copy").toggleClass('fadeIn');
-    $("#copy").toggleClass('fadeOut');
+
+
     $(".container").toggleClass('no-sidebar');
   });
 
@@ -20,6 +43,12 @@ $(document).ready(function () {
     }, function () {
       $("#floating-btns__info").removeClass('active');
     }
-  )
+  );
+
+  $(".item").click(function () {
+    activeItem.removeClass("active");
+    $(this).addClass("active");
+    activeItem = $(this);
+  });
 
 });
