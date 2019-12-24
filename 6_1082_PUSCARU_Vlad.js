82d8c355523070c88c1efca0fa7db57fbe0bc4d8
Vlad-Cristian Puscaru <vlad.cpuscaru@gmail.com>
2019-12-24 17:37:01 +0200

    bug fix

--- 6_1082_PUSCARU_Vlad.js
+++ 6_1082_PUSCARU_Vlad.js
@@ -9,22 +9,27 @@ $(document).ready(function () {
         $("#info").removeClass('animated--fast fadeOut');
         $("#copy").removeClass('animated--fast fadeOut');
         $("#toolbar").removeClass('animated--fast fadeOut');
+        $("#selected-properties").removeClass('animated--fast fadeOut');
         $("#info").addClass('animated fadeIn active');
         $("#copy").addClass('animated fadeIn active');
         $("#toolbar").addClass('animated fadeIn active');
+        $("#selected-properties").addClass('animated fadeIn active');
       }, 500);
     } else {
       setTimeout(function () {
         $("#info").removeClass('active');
         $("#copy").removeClass('active');
         $("#toolbar").removeClass('active');
+        $("#selected-properties").removeClass('active');
       }, 500);
       $("#info").removeClass('animated fadeIn');
       $("#copy").removeClass('animated fadeIn');
       $("#toolbar").removeClass('animated fadeIn');
+      $("#selected-properties").removeClass('animated fadeIn');
       $("#info").addClass('animated--fast fadeOut');
       $("#copy").addClass('animated--fast fadeOut');
       $("#toolbar").addClass('animated--fast fadeOut');
+      $("#selected-properties").addClass('animated--fast fadeOut');
     }
 
     $(this).toggleClass('rotated');
